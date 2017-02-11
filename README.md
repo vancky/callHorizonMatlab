0633ecefa6a962942021cade38c6c1e923488301
vancky <vanckyli@gmail.com>
2017-02-11 12:38:36 +0800

    first version of query online

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # callHorizonMatlab
 call horizon online matlab version
+a matlab version of [callhorizons](https://github.com/mommermi/callhorizons) 
--- queryHorizon.m
+++ queryHorizon.m
@@ -1,38 +1,28 @@
-classdef query
+classdef queryHorizon
     properties
         targetname
-        not_smallbody=1;
-        cap=1;
+        official_name
         start_epoch
         stop_epoch
         step_size
         discreteepochs
         url
         data
+        physical
+        originSrc
     end
     
     methods % constructor 
-        function self = query(targetname,smallbody,cap)
+        function self = queryHorizon(targetname)
             %{
         Initialize query to Horizons
         Parameters
         ----------
         targetname         : str
            HORIZONS-readable target number, name, or designation
-        smallbody          : boolean
-           use ``smallbody=0`` if targetname is a planet or spacecraft (optional, default: True)
-        cal                : boolean
-           set to `True` to return the current apparition for comet targets.
         Results
         self
             %}
-            if nargin==1
-                self.not_smallbody=1;
-                self.cap=1;
-            else
-                self.not_smallbody= ~smallbody;
-                self.cap            = cap;
-            end
             self.targetname = targetname;
             self.start_epoch    = nan;
             self.stop_epoch     = nan;
@@ -60,9 +50,8 @@ classdef query
         
         Examples
         --------
-        >>> import callhorizons
-        >>> ceres = callhorizons.query('Ceres')
-        >>> ceres.set_epochrange('2016-02-26', '2016-10-25', '1d')
+        >>> ceres =queryHorizon('Ceres')
+        >>> ceres=ceres.set_epochrange('2016-02-26', '2016-10-25', '1d')
         Note that dates are mandatory; if no time is given, midnight is assumed.
         %}
         self.start_epoch = start_epoch;
@@ -76,18 +65,14 @@ classdef query
         Parameters
         ----------
         discreteepochs    : list
-           list of floats or strings, maximum length: 15
         Returns
         -------
         None
         
         Examples
         --------
-        >>> import callhorizons
-        >>> ceres = callhorizons.query('Ceres')
+        >>> ceres = queryHorizon('Ceres')
         >>> ceres.set_discreteepochs([2457446.177083, 2457446.182343])
-        
-        If more than 15 epochs are provided, the list will be cropped to 15 epochs.
         %}
         self.discreteepochs = discreteepochs;
         end
@@ -97,20 +82,21 @@ classdef query
         dates
         queryUrl
         dates_jd
+        ephochNo
     end
     methods
         function tt=get.fields(self)
             % returns list of available properties for all epochs
             try
-                tt=self.data.dtype.names;
+                tt=self.data.Properties.VariableNames;
             catch
                 tt=[];
             end
         end
-        function tt=ephochNo(self)
+        function tt=get.ephochNo(self)
             % returns total number of epochs that have been queried
             try
-                tt=int(self.data.shape(1));
+                tt=size(self.data,1);
             catch
                 tt=0;
             end
@@ -118,7 +104,7 @@ classdef query
         function tt=get.dates(self)
             % returns list of epochs that have been queried (format 'YYYY-MM-DD HH-MM-SS')
             try
-                tt=self.data('datetime');
+                tt=self.data{:,'datetime'};
             catch
                 tt=[];
             end
@@ -134,7 +120,7 @@ classdef query
         function tt=get.dates_jd(self)
             % returns list of epochs that have been queried (Julian Dates)
             try
-                tt=self.data('datetime_jd');
+                tt=self.data{:,'datetime_jd'};
             catch
                 tt=[];
             end
@@ -143,7 +129,7 @@ classdef query
             %returns brief query information
             tt=sprintf('<callhorizons.query object: %s>',self.targetname);
         end
-        function tt=getitem(self,key)
+        function tt=getitem(self,key,k)
             %{
             provides access to query data
 
@@ -157,11 +143,18 @@ classdef query
         query data according to key
 
             %}
-            if isnan(self.data)||isempty(self.data)
+            
+            if isempty(self.data)
                 disp('CALLHORIZONS ERROR: run get_ephemerides or get_elements first');
                 tt=nan;
             else
-                tt=self.data(key);
+                if nargin>2&&max(k)<=self.ephochNo&&min(k)>0
+                    tt=self.data{k,key};
+                elseif nargin>2&&(max(k)>self.ephochNo||min(k)<0)&&k~=':'
+                     error('out of index')
+                else
+                    tt=self.data{:,key};
+                end
             end
         end
     end
@@ -184,21 +177,20 @@ classdef query
         skip_daylight        : boolean
            crop daylight epoch during query (optional)
         
-        Results
-        -------
-        number of epochs queried
-        
         Examples
         --------
-        >>> ceres = callhorizons.query('Ceres')
-        >>> ceres.set_epochrange('2016-02-23 00:00', '2016-02-24 00:00', '1h')
-        >>> print (ceres.get_ephemerides(568), 'epochs queried')
+        >>> ceres = queryHorizon('Ceres')
+        >>> ceres=ceres.set_epochrange('2016-02-23 00:00', '2016-02-24 00:00', '1h')
+        >>> cerer=ceres.get_ephemerides('O44');
+        >>> ceres.getitem('RA',1)
+        >>> ceres.data(:,'RA')
+        >>> ceres.data{:,'RA'}
 
         The queried properties and their definitions are:
            +------------------+-----------------------------------------------+
            | Property         | Definition                                    |
            +==================+===============================================+
-           | targetname       | official number, name, designation [string]   |
+           |official_name     | official number, name, designation [string]   |
            +------------------+-----------------------------------------------+
            | H                | absolute magnitude in V band (float, mag)     |
            +------------------+-----------------------------------------------+
@@ -291,31 +283,78 @@ classdef query
             quantities = '1,3,4,8,9,10,18,19,20,21,23,24,27,31,33,36';
             % encode objectname for use in URL
             objectname = self.targetname;
-            
+            if ischar(observatory_code)
+            else
+                observatory_code=int2str(observatory_code);
+            end
             tmpurl=strcat('http://ssd.jpl.nasa.gov/horizons_batch.cgi?batch=l',...
                 '&TABLE_TYPE=''OBSERVER''','&QUANTITIES=''',quantities,'''',...
             '&CSV_FORMAT=''YES''','&ANG_FORMAT=''DEG''','&CAL_FORMAT=''BOTH''',...
-            '&SOLAR_ELONG=''',sprintf('%d,%d',solar_elongation),...
-            sprintf('%s%d''','&CENTER=''',observatory_code),'&COMMAND=',objectname);
+            '&SOLAR_ELONG=''',sprintf('%d,%d''',solar_elongation),...
+            sprintf('&CENTER=''%s''&COMMAND=''%s''',observatory_code,objectname));
             if ~isnan(self.discreteepochs)
                 tmpurl=strcat(tmpurl,'&TLIST=');
                 for k=1:length(self.discreteepochs)
-                    tmpurl=strcat(tmpurl,sprintf('''%s''',self.discreteepochs(k)));
+                    tmpurl=strcat(tmpurl,sprintf('''%f''',self.discreteepochs(k)));
                 end
-            elseif ~(isnan(self.start_epoch)||isnan(self.stop_epoch)||isnan(self.step_size))
+            elseif ~(isempty(self.start_epoch)||isempty(self.stop_epoch)||isempty(self.step_size))
                 tmpurl=strcat(tmpurl,sprintf('&START_TIME=''%s''&STOP_TIME=''%s''&STEP_SIZE=''%s''',...
                     self.start_epoch,self.stop_epoch,self.step_size));
             else
                 error('no epoch information');
             end
-            tmpurl=strcat(tmpurl,sprintf('&AIRMASS=%d''',min(airmass_lessthan,99)));
+            tmpurl=strcat(tmpurl,sprintf('&AIRMASS=''%d''',min(airmass_lessthan,99)));
             if skip_daylight
                 tmpurl=strcat(tmpurl,'&SKIP_DAYLT=''YES''');
             else
                 tmpurl=strcat(tmpurl,'&SKIP_DAYLT=''NO''');
             end
             self.url=tmpurl;
-            disp(self.url);
+         %   disp(self.url);
+            src=urlread(self.url);
+           
+            % get website source and resolve it into data
+            % get H and G
+            if isempty(regexp(src,' H= ','ONCE','start'))
+                H=nan;G=nan;
+            else
+                H=str2double(src(regexp(src,' H= ','end','ONCE'):regexp(src,' G= ','start','ONCE')));
+                G=str2double(src(regexp(src,' G= ','end','ONCE'):regexp(src,' B-V= ','start','ONCE')));
+            end
+            self.physical.H=H;
+            self.physical.G=G;
+            self.originSrc=src;
+            % get target name 
+             if ~isempty(regexp(src,'Multiple major-bodies match string|No matches found','ONCE'))
+                error('Ambiguous target name; check URL: %s' ,self.url);
+            end
+            if ~isempty(regexp(src,'Matching small-bodies|No matches found','ONCE'))
+                error('Ambiguous target name; check URL: %s' ,self.url);
+            end
+            if ~isempty(regexp(src,'ERROR','ONCE'))
+                error('%s' ,src);
+            end
+            pos1=regexp(src,'Target body name: ','end','ONCE')+1;
+            pos2=pos1+regexp(src(pos1:pos1+38),'  ','start','ONCE')-2;
+            self.official_name=src(pos1:pos2);
+            % not support multiple bodies
+           
+            % get header 
+         %   pos3=regexp(src,'Date...UT...HR.MN','ONCE');
+           % C=textscan(src(pos3:end),'%s',33,'Delimiter',',');
+          %  fieldnames=C{1};
+            fieldnames={'datetime','datetime_jd','solar_presence','lunar_presence',...
+              'RA','DEC','RA_rate','DEC_rate','AZ','EL','airmass','magextinct','V',...
+             'S_brt', 'illumination','EclLon','EclLat','r','r_rate','delta','delta_rate',...
+             'lighttime','SOT','relativeSun','STO','sunTargetPA','velocityPA','ObsEclLon',...
+             'ObsEclLat','GlxLon','GlxLat','RA_3sigma','DEC_3sigma'};
+            % get ephemrides
+            pos4=regexp(src,'\$\$SOE\n','ONCE','end');
+            pos5=regexp(src,'\n\$\$EOE','ONCE','start');
+            pattern=['%s%f%s%s',repmat('%f',[1,6]),'%s%s',repmat('%f',[1,11]),'%s',repmat('%f',[1,9])];
+            C=textscan(src(pos4:pos5),pattern,'Delimiter',',');
+            self.data=table(C{1:end},'VariableNames',fieldnames);
+           
         end
     end
     
